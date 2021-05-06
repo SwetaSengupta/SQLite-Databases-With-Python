7a76d6c34638a24a8f28c9889a44a41f1d6ea024
sweta sengupta <ssengupta801@gmail.com>
2021-05-06 15:17:56 -0500

    instantiated a cursor and the created a table

--- db.py
+++ db.py
@@ -1,3 +1,16 @@
 import sqlite3
 conn = sqlite3.connect('customer.db')#creating and connecting to db
 #conn = sqlite3.connect(':memory')# connecting to an existing database
+c = conn.cursor() # create the cursor
+#create a table
+c.execute("""CREATE TABLE customers(
+    first_name text,
+    last_name text,
+    email text
+    )""")
+conn.commit() #commit our command
+conn.close()# closing the connection
+
+
+
+
